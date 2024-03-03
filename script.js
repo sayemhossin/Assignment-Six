998918b4019d2038ee755fbd2a8fb5af0c4a4790
sayemhossin <sayemhossin22@gmail.com>
2024-03-03 16:20:19 +0600

    added search by value

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ const cardDiv = document.getElementById('card-div')
 const cardDiv2 = document.getElementById('card-div-two')
 
 
-const allCard = async() =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+const allCard = async(id) =>{
+ document.getElementById('spinner').style.display='block'
+    const res = await fetch(` https://openapi.programming-hero.com/api/retro-forum/posts?category=${id}`)
     const data = await res.json()
+         cardDiv.innerHTML=''
+
     data.posts.forEach(card =>{
+
+      document.getElementById('spinner').style.display='none'
+
        const div = document.createElement('div')
-      
+      div.style.display='flex'
        div.innerHTML=`
-       <div  class="flex flex-col lg:flex-row flex-1 bg-[#797DFC1A] gap-5 p-7 rounded-md">
+       <div  class="flex flex-1 flex-col lg:flex-row  bg-[#797DFC1A] gap-5 p-7 rounded-md">
     <div>
-    <div class="ok avatar online">
+    <div class=" avatar online">
     <div class="w-24 rounded-full">
       <img src=" ${card.image}" />
     </div>
+ </div>
  </div>
  <div class="space-y-6">
      <div class="flex gap-14">
@@ -38,7 +45,6 @@ title}</h1>
          <img id="img" src="images/Group 16.png" alt="">
          <p> ${card.view_count}</p>
         </div>
-
         <div class="flex gap-2 items-center">
         <img id="img" src="images/Group 18.png" alt="">
          <p>${card.posted_time} Min</p>
@@ -46,7 +52,7 @@ title}</h1>
 
          </div>
          <div>
-             <button onclick="button('${card.title}  ${card.view_count}')"><img src="images/Group 40106.jpeg" alt="">
+             <button onclick="button('${card.title}... / views: ${card.view_count}')"><img src="images/Group 40106.jpeg" alt="">
              </button>
          </div>
 
@@ -66,40 +72,46 @@ cardDiv.appendChild(div)
 
 let count = 1
 function button(e){
-   const number = document.getElementById('count').innerText=count
+   const number = document.getElementById('count').innerText = count
    count++
    const name = document.getElementById('name')
 const p = document.createElement('p')
-// p.innerText = title.textContent
+p.style.padding='10px'
+p.style.paddingTop='20px'
+p.style.paddingBottom='20px'
+p.style.fontSize='25px'
+p.style.fontWeight='bold'
+p.style.boxShadow='2px 2px 2px 2px black'
 p.innerText = e
 name.appendChild(p)
 }
 
 
-// card section 2'
+
+// card section 2
 const cardTwo = async() =>{
    const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/latest-posts')
    const data = await res.json()
-   data.forEach(card =>{
+   data.forEach(card2 =>{
    const div = document.createElement('div')
    div.innerHTML=`
    <div class="card bg-base-100 shadow-xl px-4">
-    <figure><img class="rounded-2xl" src="${card.cover_image}" alt="Shoes" /></figure>
+    <figure><img class="rounded-2xl" src="${card2.cover_image}" alt="Shoes" /></figure>
     <div class="py-2 space-y-3">
-      <p>${card.author.posted_date}</p>
-      <h2 class="card-title font-extrabold">${card.title}</h2>
-      <p>${card.description}</p>
+      <p>${card2.author.posted_date}</p>
+      <h2 class="card-title font-extrabold">${card2.title}</h2>
+      <p>${card2.description}</p>
       <div class="flex gap-10">
         <div class="avatar">
           <div class="w-16 rounded-full">
-            <img src="${card.profile_image}" />
+            <img src="${card2.profile_image}" />
           </div>
 
         </div>
 
        <div>
-        <h2 class="font-extrabold">${card.author.name}</h2>
-        <p>${card.author.designation}</p>
+        <h2 class="font-extrabold">${card2.author.name}</h2>
+        <p>${card2.author.designation}</p>
       </div>
       </div>
     </div>
@@ -112,5 +124,17 @@ const cardTwo = async() =>{
    })
 }
 
+
+const handleSearch = () =>{
+  const input =  document.getElementById('input').value
+  if(input){
+    allCard(input)
+}else{
+   alert("write what do you need!!")
+}
+document.getElementById('input').value =''
+  
+}
+
 cardTwo()
-allCard()
+allCard('comedy')
